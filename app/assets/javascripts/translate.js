7a616ed0dcc32f69ce1ddd13d665c7e2d1354042
ArioShaman <ariosha6@gmail.com>
2018-01-30 00:15:19 +0300

    Content

--- app/assets/javascripts/translate.js
+++ app/assets/javascripts/translate.js
@@ -6,7 +6,17 @@ var translationsEN = {
   'CONTACTS': 'Contacts',
   'LK': 'Loged in',
   'LOGIN': 'Login',
-  'LOG': 'Log in'
+  'LOG': 'Log in',
+  'desc': 'Description:',
+  'status': 'Status:',
+  'goto': 'Go to',
+  'done': 'Done',
+  'inDev': 'In development',  
+  'martDesc': 
+      'Landing-page template for a branded online clothing store.'+
+      ' In addition to the well-chosen corporate identity, the site is adapted to'+
+      ' different Devices from desperate to mobile browsers',
+  'madDesc':"Team's pet-project, developed for yourself. The goal of the project is to get data on popular music charts for every year 21st in each country. And give the user an interactive way to get this data and listen to the songs of these charts",
 };
 
 var translationsRU = {
@@ -18,4 +28,14 @@ var translationsRU = {
   'LK':'ЛИЧНЫЙ КАБИНЕТ',
   'LOGIN': 'АВТОРИЗАЦИЯ',
   'LOG': 'ВОЙТИ',
+  'desc': 'Описание:',
+  'status': 'Статус:',
+  'goto': 'ПЕРЕЙТИ',
+  'done': 'Готово',
+  'inDev': 'В разработке',
+  'martDesc': 
+      'Шаблон Landing-page для брендового интернет-магазина одежды.'+
+      ' Помимо отлично подобранного фирменного стиля, сайт адаптирован под различные '+
+      'устройства от дестопных до мобильных браузеров',
+  'madDesc':'Домашний проект команды, разрабатываемый для себя. Цель проекта получить данные о популярных музыкальных чартах за каждый год 21 ст в каждой стране. И дать пользователю интерактивно получить эти данные и прсослушать песни этиъ чартов',
 };
